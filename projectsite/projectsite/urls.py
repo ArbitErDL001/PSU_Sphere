88fa71fb27358c9d9ca2fb0a036ad789a925dae0
ArbitErDL001 <202380330@psu.palawan.edu.ph>
2026-08-19 22:09:47 +0800

    static bootstrap

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from studentorg.views import HomePageView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('index.html', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('components.html', TemplateView.as_view(template_name='components.html'), name='components'),
+    path('forms.html', TemplateView.as_view(template_name='forms.html'), name='forms'),
+    path('tables.html', TemplateView.as_view(template_name='tables.html'), name='tables'),
+    path('notifications.html', TemplateView.as_view(template_name='notifications.html'), name='notifications'),
+    path('typography.html', TemplateView.as_view(template_name='typography.html'), name='typography'),
+    path('icons.html', TemplateView.as_view(template_name='icons.html'), name='icons'),
     path('admin/', admin.site.urls),
 ]
